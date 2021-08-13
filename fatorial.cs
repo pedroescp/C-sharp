36c5c3f3c20d66f09fd95c2bbc6c7b5c3716f6d4
Pedro <69729572+pedroescp@users.noreply.github.com>
2021-08-13 20:57:14 -0300

    Add files via upload

---
+++ fatorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+					
+public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("digite o numero para repetidor");
+        Int32 contador = Convert.ToInt32(Console.ReadLine());
+        
+        int fatorial = 1;
+        for (int repetidor = 1; repetidor <= contador; repetidor++)
+        {
+            fatorial*=repetidor;
+            Console.WriteLine(repetidor+" fatorial= "+fatorial);
+        }
+    }
+}

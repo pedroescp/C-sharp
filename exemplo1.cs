f4c9e21f3ee865aa0ff2b612a88e379090a8639c
Pedro <69729572+pedroescp@users.noreply.github.com>
2021-08-20 21:48:07 -0300

    Add files via upload

---
+++ exemplo1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        /*Instruções
+        Faça um programa em C# que lê um número inteiro positivo, que representa a quantidade de palavras que serão lidas.
+        Ao realizar a leitura de cada uma das palavras, o sistema deverá concatená-las utilizando o operador de soma, e através do StringBuilder.
+        O programa deverá imprimir as palavras separadas por vírgula (não imprimir uma vírgula no final).
+        Exemplo:
+        Usuário informa o número 6
+        Sistema faz a leitura de seis palavras
+        Sistema imprime as palavras:
+        Palavra 1, Palavra 2, Palavra 3, Palavra 4, Palavra 5, Palavra 6*/
+
+        StringBuilder frase = new StringBuilder();
+        
+        Console.WriteLine("Digite quantidade de vezes a ser repetida");
+        Int32 numero = Convert.ToInt32(Console.ReadLine()); //recebe numero
+        
+        if (numero > 0)
+        {
+
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine("Digite a palavra : " + i);
+                string palavra = Convert.ToString(Console.ReadLine());//recebe palavra
+
+                if (numero-1   ==  i )
+                {
+                    frase.Append(palavra);
+                    break;
+                }
+                
+                frase.Append($"{palavra}, ");
+            }
+            
+            Console.WriteLine(frase.ToString());
+            
+        }
+        else
+        {
+            Console.WriteLine("Escreva um numero positivo");
+        }
+
+
+    }
+}
---
+++ exercicio2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        /* Instruções
+        
+        Ao realizar a leitura de cada uma das palavras, o programa deverá verificar qual delas é a menor e qual delas é a maior (ordem alfabética, e não pelo tamanho).
+        Após realizar a leitura de todas as palavras, o programa deverá imprimir a menor com todas as letras minúsculas, e a maior com todas as letras maiúsculas.
+        Exemplo:
+        Usuário informa o número 4
+        O programa faz a leitura das palavras:
+        Maria, Ana, Paula, Bianca
+        O programa imprime a menor e a maior:
+        ana
+        PAULA*/
+
+        // Console.WriteLine("bbbbbbbbbbbbbbbb".CompareTo("AAAAAAAAAA"));// maior
+        // Console.WriteLine("AAAAAA".CompareTo("BBBBBBBBB")); //menor
+        // Console.WriteLine("AAAAAA".CompareTo("AAAAAA")); //igual
+        
+        
+        
+        Console.WriteLine("Digite quantidade de vezes a ser repetida");
+        Int32 numero = Convert.ToInt32(Console.ReadLine()); //recebe numero
+
+        string menor = "zzzz";
+        string maior = "aaaa";
+        
+        if (numero > 0)
+        {
+
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine("Digite a palavra : " + i);
+                string palavra = Convert.ToString(Console.ReadLine());//recebe palavra
+
+                if (palavra.CompareTo(maior) == 1)
+                {
+                    maior = palavra;
+                }
+
+                if (palavra.CompareTo(menor) == -1)
+                {
+                    menor = palavra;
+                }
+            }
+
+            Console.WriteLine($"A maior palavra é: {maior.ToUpper()}");
+            Console.WriteLine($"A menor palavra é: {menor.ToLower()}");
+        }
+        else
+        {
+            Console.WriteLine("Escreva um numero positivo");
+        }
+
+
+    }
+}
